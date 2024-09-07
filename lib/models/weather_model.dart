b80635a7d85642fa09953be6efaa14ac5f20bc5c
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-09-07 14:08:28 +0530

    Created WeatherModel to structure weather data from API

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,26 @@
+class WeatherModel {
+  final String cityName;
+  final double temperature;
+  final String description;
+  final int humidity;
+  final int rainChances;
+  final String weatherIcon;
+
+  WeatherModel(
+      {required this.cityName,
+      required this.temperature,
+      required this.description,
+      required this.humidity,
+      required this.rainChances,
+      required this.weatherIcon});
+
+  factory WeatherModel.fromJSON(Map<String, dynamic> json) {
+    return WeatherModel(
+        cityName: json['name'],
+        temperature: json['main']['temp'],
+        description: json['weather'][0]['description'],
+        humidity: json['main']['humidity'],
+        rainChances: json['main']['humidity'],
+        weatherIcon: json['weather']['0']['icon']);
+  }
+}
